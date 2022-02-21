084e2b18373a6858b0bdeb916a20bad58531376f
Alexander Sidorov <alexander@sidorov.dev>
2022-02-21 18:03:36 +0300

    excluded sentry from coverage

--- src/main/__init__.py
+++ src/main/__init__.py
@@ -2,7 +2,7 @@ import sentry_sdk
 
 from framework.config import settings
 
-if not settings.MODE_DEBUG:
+if not settings.MODE_DEBUG:  # pragma: no cover
     sentry_sdk.init(
         settings.SENTRY_DSN,
         # Set traces_sample_rate to 1.0 to capture 100%
